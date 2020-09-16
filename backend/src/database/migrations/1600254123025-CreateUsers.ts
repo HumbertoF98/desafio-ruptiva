d865e6611d39591cafae5d5ab277a11de582eda8
Humberto Filho <humbertofilho5@gmail.com>
2020-09-16 07:04:06 -0400

    criando migration de usuarios

---
+++ backend/src/database/migrations/1600254123025-CreateUsers.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export default class CreateUsers1600254123025 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "password",
+            type: "varchar",
+            isNullable: false,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
