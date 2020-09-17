d4a7dfe3fde1160aafe7898b9e1adad91ef4695e
Humberto Filho <humbertofilho5@gmail.com>
2020-09-16 21:46:24 -0400

    gerando token de usuário

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import revenuesRouter from "./revenues.routes";
+import usersRouter from "./users.routes";
+import sessionsRouter from "./sessions.routes";
 
 const routes = Router();
 
 routes.use("/revenues", revenuesRouter);
+routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 
 export default routes;
