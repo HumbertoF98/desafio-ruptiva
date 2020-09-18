54ec73428d7fe3710e880d213d41eb48a9577943
Humberto Filho <humbertofilho5@gmail.com>
2020-09-18 00:12:42 -0400

    backend finalizado

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -13,6 +13,8 @@ class UserController {
         password,
       });
 
+      delete user.password;
+
       return response.status(200).json({ user });
     } catch (err) {
       return response.status(400).json({ message: err.message });
